c7292be06087bd134a44eb128750806dba77144b
Inc0gnit076 <kmacinnis76@gmail.com>
2022-06-27 11:00:23 -0400

    add title and grids

---
+++ javascript.js
@@ -0,0 +1,10 @@
+for (let i = 0; i < 256; i++) {
+    const div = document.querySelector('div');
+    const content = document.createElement('div');
+    content.classList.add('square');
+    div.appendChild(content);
+    
+}
+
+
+
